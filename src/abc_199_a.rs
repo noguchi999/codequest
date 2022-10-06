7a0cfcb1d66de38b04276adcda56f4d5b296a016
noguchi999 <noguchi999@gmail.com>
2022-10-06 14:20:26 +0900

    [modify] some update.

--- src/abc_199_a.rs
+++ src/abc_199_a.rs
@@ -1,7 +1,12 @@
 pub fn exec() {
 
-    let mut word = String::new();
-    std::io::stdin().read_line(&mut word).ok();
-    let answer: i32 = word.trim().parse().unwrap();
-    println!("数字が1つ: {}", answer);
+    let mut word_1 = String::new();
+    std::io::stdin().read_line(&mut word_1).ok();
+    let answer_1: i32 = word_1.trim().parse().unwrap();
+    println!("数字が1つ: {}", answer_1);
+
+    let mut word_2 = String::new();
+    std::io::stdin().read_line(&mut word_2).ok();
+    let answer_2: Vec<&str> = word_2.trim().split(" ").collect();
+    println!("数字が複数: {}", answer_2.join(","));
 }
