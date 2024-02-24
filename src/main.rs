79b0190b040d60032407f5cde6aae771e5fed31a
noguchi999 <noguchi999@gmail.com>
2024-02-25 07:24:10 +0900

    [modify] maze

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-mod abc_199_a;
-mod abc_197_a;
-mod abc_154_d;
-mod abc_167_c;
-mod caesar_enc;
-mod prime100;
+// mod abc_199_a;
+// mod abc_197_a;
+// mod abc_154_d;
+// mod abc_167_c;
+// mod caesar_enc;
+// mod prime100;
+mod maze;
 
 fn main() {
     // abc_199_a::exec();
@@ -11,5 +12,6 @@ fn main() {
     // abc_154_d::exec();
     // abc_167_c::exec();
     // caesar_enc::main();
-    prime100::main();
+    // prime100::main();
+    maze::main();
 }
