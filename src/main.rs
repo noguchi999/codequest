08a22daca2183290356b98408617c3641bdcb1f8
noguchi999 <noguchi999@gmail.com>
2024-03-04 15:20:53 +0900

    [add] http_client_get_async

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@
 // mod maze;
 // mod bingo;
 // mod http_client_get;
-mod http_client_post;
+// mod http_client_post;
+mod http_client_get_async;
 
 fn main() {
     // abc_199_a::exec();
@@ -19,5 +20,6 @@ fn main() {
     // maze::main();
     // bingo::main();
     // _ = http_client_get::main();
-    _ = http_client_post::main();
+    // _ = http_client_post::main();
+    _ = http_client_get_async::main();
 }
