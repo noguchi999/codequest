35fc5c08f7484ac8ca62c5edeaaf86d998d018e7
noguchi999 <noguchi999@gmail.com>
2024-03-21 15:18:27 +0900

    [add] findfile

---
+++ src/findfile.rs
@@ -0,0 +1,17 @@
+use std::{env, path};
+
+pub fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        println!("Usage: findfile <path> <keyword>");
+        return;
+    }
+
+    let target_dir = &args[1];
+    let keyword = &args[2];
+    let target = path::PathBuf::from(target_dir);
+    findfile(&target, keyword);
+}
+
+fn findfile(target: path::PathBuf, keyword: &str) {
+}
