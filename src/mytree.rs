d3e7d4e8d2df8676e447626afc807f75ea1796d6
noguchi999 <noguchi999@gmail.com>
2024-03-24 06:25:39 +0900

    [add] tree

---
+++ src/mytree.rs
@@ -0,0 +1,17 @@
+use std::{env, path};
+
+pub fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut target_dir = ".";
+    if args.len() >= 2 {
+        target_dir = &args[1];
+    }
+
+    let target = path::PathBuf::from(target_dir);
+    println!("{}", target_dir);
+    tree(&target, 0);
+}
+
+fn tree(target: &path::PathBuf, level: isize) {
+
+}
