93d25d0af4cdd5077591d6434c22354f2e527631
noguchi999 <noguchi999@gmail.com>
2024-04-03 15:57:33 +0900

    [add] bim_struct

---
+++ src/bmi_struct.rs
@@ -0,0 +1,19 @@
+struct BmiRange {
+    min: f64,
+    max: f64,
+    label: &'static str,
+}
+
+pub fn main() {
+    let height_cm = input("身長(cm)は？");
+    let weight_kg = input("体重(kg)は？");
+
+    let height = height_cm / 100.0;
+    let bmi = weight_kg / height.powf(2.0);
+
+    let bmi_list = vec![
+        BmiRange { min: 0.0, max: 18.5, label: "低体重" },
+        BmiRange { min: 18.5, max: 25.0, label: "普通体重" },
+        BmiRange { min: 25.0, max: 30.0, label: "肥満1度"},
+    ];
+}
