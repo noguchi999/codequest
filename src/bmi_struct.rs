6ba6f23b54471cd50de805389af896d8716c3976
noguchi999 <noguchi999@gmail.com>
2024-04-03 16:50:11 +0900

    [modify] bmi_struct

--- src/bmi_struct.rs
+++ src/bmi_struct.rs
@@ -14,6 +14,6 @@ pub fn main() {
     let bmi_list = vec![
         BmiRange { min: 0.0, max: 18.5, label: "低体重" },
         BmiRange { min: 18.5, max: 25.0, label: "普通体重" },
-        BmiRange { min: 25.0, max: 30.0, label: "肥満1度"},
+        BmiRange { min: 25.0, max: 30.0, label: "肥満1度" },
     ];
 }
