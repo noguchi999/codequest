101d3e13041cb89acd26693599b5e822467253d7
noguchi999 <noguchi999@gmail.com>
2024-04-16 15:02:17 +0900

    [modify] trait_treasure_box_def

--- src/trait_treasure_box_def.rs
+++ src/trait_treasure_box_def.rs
@@ -6,3 +6,30 @@ trait TreasureBox {
     fn check(&self);
     fn get_key_no(&self) -> i32;
 }
+
+struct JewelBox {
+    price: i32,
+    key_no: i32,
+}
+impl TreasureBox for JewelBox {
+    fn check(&self) {
+        println!("宝石箱だった! 金貨{}枚を入手。", self.price);
+    }
+
+    fn get_key_no(&self) -> i32 {
+        self.key_no
+    }
+}
+
+struct EmptyBox {
+    key_no: i32,
+}
+impl TreasureBox for EmptyBox {
+    fn check(&self) {
+        println!("空箱だった。");
+    }
+
+    fn get_key_no(&self) -> i32 {
+        self.key_no
+    }
+}
