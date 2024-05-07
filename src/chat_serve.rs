e921a0956b5a27528cca4726c885abf37a80a64a
noguchi999 <noguchi999@gmail.com>
2024-05-07 15:16:32 +0900

    [modify] chat_serve

--- src/chat_serve.rs
+++ src/chat_serve.rs
@@ -1,9 +1,11 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::{BufReader, BufReade, Wite};
+use std::io::{BufReader, BufRead, Write};
 use std::thread;
 use std::sync::mpsc;
 use std::time::Duration;
 
 pub fn main() {
     let server_addr = "127.0.0.1:8888";
+    let (tx, rx) = mpsc::channel::<String>();
+    let mut clients: Vec<TcpStream> = Vec::new();
 }
