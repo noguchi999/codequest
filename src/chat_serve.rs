8dca0b3b7435b0f5afc75ca1db1f30d4c6859f9b
noguchi999 <noguchi999@gmail.com>
2024-05-08 17:16:44 +0900

    [modify] chat_serve

--- src/chat_serve.rs
+++ src/chat_serve.rs
@@ -8,4 +8,8 @@ pub fn main() {
     let server_addr = "127.0.0.1:8888";
     let (tx, rx) = mpsc::channel::<String>();
     let mut clients: Vec<TcpStream> = Vec::new();
+
+    let server = TcpListener::bind(server_addr).expect("サーバの起動に失敗しました.");
+    server.set_nonblocking(true).expect("利用できませんでした.");
+    print!("サーバを起動しました. {}\n", server_addr);
 }
