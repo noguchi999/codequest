a3f788c4c90efb894afa7f0ac1e812efc443b5b8
noguchi999 <noguchi999@gmail.com>
2024-05-09 16:50:21 +0900

    [modify] chat_serve

--- src/chat_serve.rs
+++ src/chat_serve.rs
@@ -4,6 +4,8 @@ use std::thread;
 use std::sync::mpsc;
 use std::time::Duration;
 
+use hyper::client;
+
 pub fn main() {
     let server_addr = "127.0.0.1:8888";
     let (tx, rx) = mpsc::channel::<String>();
@@ -12,4 +14,16 @@ pub fn main() {
     let server = TcpListener::bind(server_addr).expect("サーバの起動に失敗しました.");
     server.set_nonblocking(true).expect("利用できませんでした.");
     print!("サーバを起動しました. {}\n", server_addr);
+
+    loop {
+        if let Ok((client, addr)) = server.accept() {
+            print!("{} が接続しました.\n", addr);
+            clients.push(client.try_clone().unwrap());
+            start_thread(client, tx.clone());
+        }
+    }
+}
+
+fn start_thread(client: TcpStream, tx: mpsc::Sender<String>) {
+    let mut reader = BufReader::new(client);
 }
