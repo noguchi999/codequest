9dc93339466843f520bc6a088bc1dd4d59adbdb9
noguchi999 <noguchi999@gmail.com>
2024-05-14 16:25:50 +0900

    [modify] chat_client

--- src/chat_client.rs
+++ src/chat_client.rs
@@ -5,4 +5,7 @@ use std::time::Duration;
 
 pub fn main() {
     let server_addr = "127.0.0.1:8880";
+    let mut socket = TcpStream::connect(server_addr).expect("Failed to connect to server");
+    socket.set_nonblocking(true).expect("Failed to set non-blocking mode");
+    println!("Connected to server at {}", server_addr);
 }
