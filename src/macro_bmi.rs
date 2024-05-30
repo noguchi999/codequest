cf49dccc57c3138ec9776d4438c07b2b15b63225
noguchi999 <noguchi999@gmail.com>
2024-05-30 17:38:16 +0900

    [modify] macro_bmi

--- src/macro_bmi.rs
+++ src/macro_bmi.rs
@@ -14,4 +14,11 @@ pub fn main() {
     let h: f32 = 158.0;
     let w: f32 = 50.0;
     let bmi = w / (h / 100.0).powf(2.0);
+    let lavel = bmi_select![
+        bmi;
+        "Underweight" => 0.0..18.5;
+        "Normal" => 18.5..25.0;
+        "Overweight" => 25.0..30.0;
+        "Obese" => 30.0..std::f32::INFINITY;
+    ];
 }
