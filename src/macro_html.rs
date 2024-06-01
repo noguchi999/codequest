8635a4cc56a9fc43971b71f00cdefd374753cb1c
noguchi999 <noguchi999@gmail.com>
2024-06-01 19:41:05 +0900

    [modify] macro_html

--- src/macro_html.rs
+++ src/macro_html.rs
@@ -1,4 +1,24 @@
 macro_rules! out_html {
     () => {()};
     ($e:tt) => {println!("{}", $e)};
+    ($tag:ident [$($inner:tt)*] $($rest:tt)*) => {{
+        print!("<{}>", stringify!($tag));
+        out_html!($($inner)*);
+        print!("</{}>", stringify!($tag));
+        out_html!($($rest)*);
+    }};
+}
+
+pub fn main() {
+    out_html! {
+        html [
+            head [
+                title ["Hello, world!"]
+            ]
+            body [
+                h1 ["Hello, world!"]
+                p ["This is a paragraph."]
+            ]
+        ]
+    }
 }
