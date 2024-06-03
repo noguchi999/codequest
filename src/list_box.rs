2daf7eb4c483a3fad83d1fc181f2379b69a37a16
noguchi999 <noguchi999@gmail.com>
2024-06-03 16:28:00 +0900

    [modify] list_box

--- src/list_box.rs
+++ src/list_box.rs
@@ -2,3 +2,7 @@ pub struct Node {
     data: i64,
     link: Option<Box<Node>>
 }
+
+fn node(v: i64, link: Option<Box<Node>>) -> Option<Box<Node>> {
+    Some(Box::new(Node { data: v, link: link }))
+}
