0166eb4451e77531df2900e5ef6c43e62617d7f4
noguchi999 <noguchi999@gmail.com>
2024-06-04 17:08:05 +0900

    [modify] list_box

--- src/list_box.rs
+++ src/list_box.rs
@@ -6,3 +6,15 @@ pub struct Node {
 fn node(v: i64, link: Option<Box<Node>>) -> Option<Box<Node>> {
     Some(Box::new(Node { data: v, link: link }))
 }
+
+pub fn main() {
+    let c = ndoe(10, node(20, node(30, None))).unwrap();
+    let mut p = &c;
+    loop {
+        println!("{}", p.data);
+        match p.link {
+            Some(ref link) => p = &link,
+            None => break
+        }
+    }
+}
