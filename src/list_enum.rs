57efcab14ab9a5755046b2c7621f6518d0589bba
noguchi999 <noguchi999@gmail.com>
2024-06-07 18:31:18 +0900

    [modify] list_enum

--- src/list_enum.rs
+++ src/list_enum.rs
@@ -7,3 +7,18 @@ use Node::{Empty, Cons};
 fn node(v: i64, link: Box<Node>) -> Box<Node> {
     Box::new(Cons(v, link))
 }
+
+pub fn main() {
+    let c = node(10, node(20, node(30, Box::new(Empty))));
+    let mut ptr: &Box<Node> = &c;
+    loop {
+        let cur_node: &Node = &**ptr;
+        match cur_node {
+            Empty => break,
+            Cons(v, link) => {
+                println!("{}", v);
+                ptr = &link;
+            }
+        }
+    }
+}
