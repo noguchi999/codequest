929189ae0cd7e8686f7a9165bcd4209fcd62795c
noguchi999 <noguchi999@gmail.com>
2024-06-11 15:02:43 +0900

    [modify] s_list

--- src/s_list.rs
+++ src/s_list.rs
@@ -6,3 +6,14 @@ pub struct Node {
 pub struct List {
     head: Option<Box<Node>>,
 }
+
+impl List {
+    pub fn new() -> Self {
+        Self { head: None }
+    }
+
+    pub fn unshift(&mut self, v:size) {
+        let new_node = Node{data: v, link: self.head.take()};
+        self.head = Some(Box::new(new_node));
+    }
+}
