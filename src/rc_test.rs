33be5fb0748ff68c7817e57691de0d18bf3e3be1
noguchi999 <noguchi999@gmail.com>
2024-06-17 14:56:03 +0900

    [modify] rc_test

--- src/rc_test.rs
+++ src/rc_test.rs
@@ -2,7 +2,11 @@ use std::rc::Rc;
 
 pub fn main() {
     let a_rc = Rc::new(1000);
-    let b_rc = Rc::clone(&a_rc);
-    println!("{}", b_rc);
-
+    {
+        let b_rc = Rc::clone(&a_rc);
+        println!("{}", b_rc);
+        println!("参照数={}", Rc::strong_count(&a_rc));
+    }
+    println!("{}", a_rc);
+    println!("参照数={}", Rc::strong_count(&a_rc));
 }
