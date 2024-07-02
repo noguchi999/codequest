0e4f6dfea1c2c1f6a33f12629c33c2d15bc47e73
noguchi999 <noguchi999@gmail.com>
2024-07-02 17:07:33 +0900

    [modify] node

--- src/node.rs
+++ src/node.rs
@@ -1,4 +1,12 @@
 #[derive(Debug, Clone)]
 pub enum Node {
-
+    Nop,
+    Number(i64),
+    Calc(char, Box<Node>, Box<Node>),
+    If(Box<Node>, Box<Vec<Node>>, Box<Vec<Node>>),
+    For(String, i64, i64, Box<Vec<Node>>),
+    Print(Box<Node>),
+    PringStr(String),
+    SetVar(String, Box<Node>),
+    GetVar(String),
 }
