12cf53384e163382a98e6fde763384d454a1ee1c
noguchi999 <noguchi999@gmail.com>
2024-07-06 19:52:42 +0900

    [modify] parser

--- src/parser.rs
+++ src/parser.rs
@@ -3,4 +3,5 @@ use crate::node::Node;
 peg::parser! (pub grammar tomat() for str {
     pub rule parse() -> Vec<Node> = v:sentences()
     rule sentences() -> Vec<Node> = sentence() ** end_of_line()
+    rule sentence() -> Node = print() / if() / for() / let() / _ {Node::Nop}
 })
