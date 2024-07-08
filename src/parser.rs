af65ac3a4a4ccd91fb0b3ddca10e2982d943018f
noguchi999 <noguchi999@gmail.com>
2024-07-08 13:51:34 +0900

    [modify] parser

--- src/parser.rs
+++ src/parser.rs
@@ -4,5 +4,9 @@ peg::parser! (pub grammar tomat() for str {
     pub rule parse() -> Vec<Node> = v:sentences()
     rule sentences() -> Vec<Node> = sentence() ** end_of_line()
     rule sentence() -> Node = print() / if() / for() / let() / _ {Node::Nop}
-    rule print() -> Node = "print" _ "\"" v:$([^ '"']*) "\""
+    rule print() -> Node
+        = "print" _ "\"" v:$([^ '"']*) "\""
+        {Node::PrintStr(v.to_string())}
+        / "print" _ v:calc()
+        {Node::Print(Box::new(v))}
 })
