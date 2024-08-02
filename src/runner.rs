af2c33d7ad16c1956763c75ed7aaea382d582373
noguchi999 <noguchi999@gmail.com>
2024-08-02 14:59:15 +0900

    [modify] runner

--- src/runner.rs
+++ src/runner.rs
@@ -11,6 +11,20 @@ fn run_node(ctx: &mut Context, node: Node) -> i64 {
         Node::Number(v) => v,
         Node::Calc(op, l, r) => {
             calc_op(op, run_node(ctx, *l), run_node(ctx, *r))
-        }
+        },
+        Node::GetVar(name) => {
+            match ctx.vars.get(&name) {
+                Some(v) => *v,
+                None => 0,
+            }
+        },
+        Node::If(cond, true_n, false_n) => {
+            let cond_v = run_node(ctx, *cond);
+            if cond_v > 0 {
+                run_node(ctx, *true_n)
+            } else {
+                run_node(ctx, *false_n)
+            }
+        },
     }
 }
