fc2a834f3695e85885c27e27fcfedc273dd0356b
noguchi999 <noguchi999@gmail.com>
2024-09-03 13:50:56 +0900

    [modify] async_test

--- async_test/src/main.rs
+++ async_test/src/main.rs
@@ -1,3 +1,10 @@
 #[tokio::main]
 async fn main() {
+    let f = say_later("Hello, world!");
+    print!("I'm doing something else...");
+    f.await;
+}
+
+async fn say_later(msg: &'static str) {
+    println!("{}", msg);
 }
