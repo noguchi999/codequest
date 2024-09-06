6b4e61e0fa6a83b5b2071f0ffd0a69438745fb5c
noguchi999 <noguchi999@gmail.com>
2024-09-06 17:10:25 +0900

    [modify] async_longtime

--- async_longtime/src/main.rs
+++ async_longtime/src/main.rs
@@ -6,3 +6,8 @@ async fn main() {
         println!("#{}を開始", i);
     }
 }
+
+async fn read_longtime() -> String {
+    time::sleep(time::Duration::from_secs(1)).await;
+    String::from("読み込み完了(fn)")
+}
