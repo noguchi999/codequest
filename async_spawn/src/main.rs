57c69ab5958f044ef47618a294009c4a26dc0bc0
noguchi999 <noguchi999@gmail.com>
2024-09-16 04:31:27 +0900

    [modify] async_spawn

--- async_spawn/src/main.rs
+++ async_spawn/src/main.rs
@@ -9,4 +9,7 @@ async fn say_later(sec: u64, msg: &str) {
 async fn main() {
     tokio::spawn(say_later(3, "毎日がエブリィデイ"));
     tokio::spawn(say_later(2, "エブリィデイがサンデー"));
+    tokio::spawn(say_later(1, "サンデーがホリデー"));
+    time::sleep(time::Duration::from_secs(4)).await;
+    println!("------");
 }
