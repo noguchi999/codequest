2e3129ecbf6786a1deb45cda7dcf7f50ed929ae5
noguchi999 <noguchi999@gmail.com>
2024-10-16 15:09:33 +0900

    [modify] cipher

--- cipher_cmd/src/main.rs
+++ cipher_cmd/src/main.rs
@@ -3,3 +3,7 @@ use block_modes::{BlockMode, Cbc, block_padding::Pkcs7};
 use sha2::{Sha256, Digest};
 
 type Aes256Cbc = Cbc<Aes256, Pkcs7>;
+const SALT: & str = "yH4dPkrXq5SkKrGmT8bYGZpXu5TxtdnM";
+
+pub fn encrypt(password: &str, data: &str) -> String {
+}
