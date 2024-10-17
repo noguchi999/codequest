e5fa381fca2eda5d54594cc09365a7a300c62518
noguchi999 <noguchi999@gmail.com>
2024-10-17 14:45:15 +0900

    [modify] cipher

--- cipher_cmd/src/main.rs
+++ cipher_cmd/src/main.rs
@@ -6,4 +6,8 @@ type Aes256Cbc = Cbc<Aes256, Pkcs7>;
 const SALT: & str = "yH4dPkrXq5SkKrGmT8bYGZpXu5TxtdnM";
 
 pub fn encrypt(password: &str, data: &str) -> String {
+    let key = get_key(password);
+}
+
+fn get_key(password: &str) -> Vec<u8> {
 }
