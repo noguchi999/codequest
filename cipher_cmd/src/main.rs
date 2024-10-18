ecd7dc4af231b744dcbe8a60ed17d4669a3a6541
noguchi999 <noguchi999@gmail.com>
2024-10-18 14:19:45 +0900

    [modify] cipher

--- cipher_cmd/src/main.rs
+++ cipher_cmd/src/main.rs
@@ -7,7 +7,11 @@ const SALT: & str = "yH4dPkrXq5SkKrGmT8bYGZpXu5TxtdnM";
 
 pub fn encrypt(password: &str, data: &str) -> String {
     let key = get_key(password);
+    let iv = get_iv();
 }
 
 fn get_key(password: &str) -> Vec<u8> {
+}
+
+fn gen_iv() -> Vec<u8> {
 }
